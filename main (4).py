7b27c717c952de95ec827f62da11c352b7c2089f
salvatoremannino-coder <salvatore.mannino@isspilimbergo.it>
2026-09-21 08:58:35 +0200

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,4 @@
+#FizzBuzz:
+#scrivere un programma che stampa  i numeri dA 1 a 100 imparte al numero
+#stampa "fizz" se il numero è divisible per 3.
+#stampa "Buzz" se il nmero è divisibile per 5 e stampa "FizzBuzz" se il numero è divisibile siA per 3 sia per 5
