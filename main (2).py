8fc25e0a476be49310cf8f6ca8399353c3102e83
salvatoremannino-coder <salvatore.mannino@isspilimbergo.it>
2026-09-21 08:49:38 +0200

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,9 @@
+i = 1
+while i <= 100:
+  if i % 2 == 0: # se è pari
+    print(str(i) + " Beep") # stampa Beep
+  else: # altrimenti e dispari
+      print(str(i)  + "Boooooooooooop") # quindi è dispari
+
+  i = i + 1
+  
