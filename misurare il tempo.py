39563120734f9841bb69aa38c5d2f7567d88490c
salvatoremannino-coder <salvatore.mannino@isspilimbergo.it>
2026-09-22 12:11:08 +0200

    Add files via upload

---
+++ misurare il tempo.py
@@ -0,0 +1,9 @@
+import time 
+food = ( "mela" , " banana" , " funghi" "topi")
+
+now = time.time()
+print(now)
+
+time.sleep(5)
+then = time.time()
+print("sono passati " + str(then - now) + " secondi ")
